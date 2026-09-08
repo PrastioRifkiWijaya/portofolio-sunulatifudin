e0a98556f2e7363b9ec8bb584fbb446e57037a31
Prastio Rifki Wijaya <wijayaprastio23@gmail.com>
2026-09-08 10:04:40 +0700

    Revert to standard Next.js for Vercel deployment

--- open-next.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({
-  incrementalCache: "dummy",
-  tagCache: "dummy",
-  queue: "dummy",
-  cachePurge: "dummy",
-});
